9a524b7406f59352d1276c39d6c0b60dbe1206c8
Ryan Walker <rawn8c@mail.umsl.edu>
2017-04-24 12:05:54 -0500

    Add project files.

---
+++ src/WoodForSheep/Models/AccountViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WoodForSheep.Models.AccountViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+    }
+}
