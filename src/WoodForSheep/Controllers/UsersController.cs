86ef901a7b47db319242b1a32959ddaf22d11947
Ryan Walker <rawn8c@mail.umsl.edu>
2017-04-27 10:16:07 -0500

    Implements basic profile page system with user libraries.

--- src/WoodForSheep/Controllers/UsersController.cs
+++ src/WoodForSheep/Controllers/UsersController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WoodForSheep.Data;
+using Microsoft.AspNetCore.Identity;
+using WoodForSheep.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +14,42 @@ namespace WoodForSheep.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: /<controller>/
+        private ApplicationDbContext context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext dbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            context = dbContext;
+        }
+
+        // GET: /Users/
         public IActionResult Index()
         {
-            return View();
+            // Returns all users.
+            IList<ApplicationUser> users = context.Users.ToList();
+
+            return View(users);
+        }
+
+        // GET: /Users/Profile
+        [HttpGet("Users/Profile/{userName}")]
+        public IActionResult Profile(string userName)
+        {
+            // Find User.
+            ApplicationUser user = context.Users.Single(u => u.UserName == userName);
+            // Find User's library.
+            // TODO: Replace with viewmodel solution.
+            ViewBag.Library = context
+                .GameUsers
+                .Include(g => g.Game)
+                .Where(u => u.UserID == user.Id)
+                .ToList();
+            return View(user);
         }
     }
 }
