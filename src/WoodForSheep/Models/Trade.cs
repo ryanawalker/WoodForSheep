8c10b46330c85efe2ee6940117ac9277bf2039d9
Ryan Walker <rawn8c@mail.umsl.edu>
2017-05-02 12:36:40 -0500

    Adds props for class references to Trade class.

--- src/WoodForSheep/Models/Trade.cs
+++ src/WoodForSheep/Models/Trade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,18 @@ namespace WoodForSheep.Models
     public class Trade
     {
         public int ID { get; set; }
+
         public int GameInitID { get; set; }
+        public Game GameInit { get; set; }
         public int GameReceiveID { get; set; }
+        public Game GameReceive { get; set; }
+
         public string UserInitID { get; set; }
-        public string UserReceiveID { get; set; }
         public string UserInitStatus { get; set; }
+        public ApplicationUser UserInit { get; set; }
+
+        public string UserReceiveID { get; set; }
         public string UserReceiveStatus { get; set; }
+        public ApplicationUser UserReceive { get; set; }
     }
 }
