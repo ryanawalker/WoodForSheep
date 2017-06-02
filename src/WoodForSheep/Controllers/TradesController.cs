0bd38815e927176f19a8c7333a6ed3c078f022c0
Ryan Walker <rawn8c@mail.umsl.edu>
2017-06-02 11:46:16 -0500

    Enables trading.

--- src/WoodForSheep/Controllers/TradesController.cs
+++ src/WoodForSheep/Controllers/TradesController.cs
@@ -76,16 +76,61 @@ namespace WoodForSheep.Controllers
             return View();
         }
 
+        // POST: /Trades/Propose
+        [HttpPost]
+        public IActionResult Propose(int gameReceiveID, int gameInitID, string userReceiveID)
+        {
+            // If user isn't logged in, redirect to login page.
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return Redirect("/Account/Login");
+            }
+
+            ClaimsPrincipal currentUser = this.User;
+            var userId = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            Trade newTrade = new Trade
+            {
+                UserInitID = userId,
+                GameInitID = gameInitID,
+                UserInitStatus = "Accepted",
+                UserReceiveID = userReceiveID,
+                GameReceiveID = gameReceiveID,
+                UserReceiveStatus = "Pending"
+            };
+
+            GameUser gameUser = context.GameUsers
+                .Single(gu => gu.GameID == gameInitID && gu.UserID == userId);
+
+            gameUser.GameStatus = "Reserved";
+
+            context.GameUsers.Update(gameUser);
+            context.Trades.Add(newTrade);
+            context.SaveChanges();
+
+            return Redirect("/Trades");
+        }
+
         // POST: /Trades/Reject
         [HttpPost]
         public IActionResult Reject(int tradeId)
         {
             Trade trade = context.Trades
                 .Single(t => t.ID == tradeId);
+            GameUser gameUserReceive = context.GameUsers
+                .Single(gu => gu.GameID == trade.GameReceiveID && gu.UserID == trade.UserReceiveID);
+            GameUser gameUserInit = context.GameUsers
+                .Single(gu => gu.GameID == trade.GameInitID && gu.UserID == trade.UserInitID);
 
             trade.UserReceiveStatus = "Rejected";
+            trade.UserInitStatus = "Rejected";
+
+            gameUserReceive.GameStatus = "Free";
+            gameUserInit.GameStatus = "Free";
 
             context.Trades.Update(trade);
+            context.GameUsers.Update(gameUserReceive);
+            context.GameUsers.Update(gameUserInit);
             context.SaveChanges();
 
             return Redirect("/Trades");
@@ -95,12 +140,50 @@ namespace WoodForSheep.Controllers
         [HttpPost]
         public IActionResult Accept(int tradeId)
         {
+            // Finds trade being accepted.
             Trade trade = context.Trades
                 .Single(t => t.ID == tradeId);
 
+            // Gets list of all currently proposed trades where the user accepting is considering trading the game involved in the current trade.
+            IList<Trade> tradesList = context.Trades
+                .Where(t => (t.GameInitID == trade.GameReceiveID && t.UserInitID == trade.UserReceiveID) || (t.GameReceiveID == trade.GameReceiveID && t.UserReceiveID == trade.UserReceiveID))
+                .Where(t => t.UserInitStatus != "Rejected" || t.UserInitStatus  != "Completed")
+                .Where(t => t.ID != tradeId)
+                .ToList();
+
+            // Iterates through list, rejecting open trades due to game being reserved for new trade, freeing up those games for their respective owners.
+            foreach (var tempTrade in tradesList)
+            {
+                tempTrade.UserInitStatus = "Rejected";
+                tempTrade.UserReceiveStatus = "Rejected";
+                
+                GameUser tempGameUserReceive = context.GameUsers
+                    .Single(gu => gu.GameID == tempTrade.GameReceiveID && gu.UserID == tempTrade.UserReceiveID);
+                GameUser tempGameUserInit = context.GameUsers
+                    .Single(gu => gu.GameID == tempTrade.GameInitID && gu.UserID == tempTrade.UserInitID);
+
+                tempGameUserInit.GameStatus = "Free";
+                tempGameUserReceive.GameStatus = "Free";
+
+                context.GameUsers.Update(tempGameUserInit);
+                context.GameUsers.Update(tempGameUserReceive);
+                context.Trades.Update(tempTrade);
+            }
+
+            // Finds GameUser object for accepted trade and user.
+            GameUser gameUserReceive = context.GameUsers
+                .Single(gu => gu.GameID == trade.GameReceiveID && gu.UserID == trade.UserReceiveID);
+            GameUser gameUserInit = context.GameUsers
+                    .Single(gu => gu.GameID == trade.GameInitID && gu.UserID == trade.UserInitID);
+
+            // Sets status of trade to accepted and reserves game in accepting user's library.
             trade.UserReceiveStatus = "Accepted";
+            trade.UserInitStatus = "Accepted";
+            gameUserReceive.GameStatus = "Reserved";
+            gameUserInit.GameStatus = "Reserved";
 
             context.Trades.Update(trade);
+            context.GameUsers.Update(gameUserReceive);
             context.SaveChanges();
 
             return Redirect("/Trades");
@@ -123,6 +206,24 @@ namespace WoodForSheep.Controllers
             else
             {
                 trade.UserReceiveStatus = "Completed";
+            }
+
+            if (trade.UserInitStatus == "Completed" && trade.UserReceiveStatus == "Completed")
+            {
+                GameUser gameUserReceive = context.GameUsers
+                    .Single(gu => gu.GameID == trade.GameReceiveID && gu.UserID == trade.UserReceiveID);
+                GameUser gameUserInit = context.GameUsers
+                    .Single(gu => gu.GameID == trade.GameInitID && gu.UserID == trade.UserInitID);
+
+                string tempUserID = gameUserReceive.UserID;
+                gameUserReceive.UserID = gameUserInit.UserID;
+                gameUserInit.UserID = tempUserID;
+
+                gameUserReceive.GameStatus = "Free";
+                gameUserInit.GameStatus = "Free";
+
+                context.GameUsers.Update(gameUserReceive);
+                context.GameUsers.Update(gameUserInit);
             }
 
             context.Trades.Update(trade);
